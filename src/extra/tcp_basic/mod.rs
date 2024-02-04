bceeee8d2dd34af8058e85f273c7691a9ee72325
securitylamb <vrishabsharma1111@gmail.com>
2024-02-04 08:24:51 +0530

    added code for tcp connection

---
+++ src/extra/tcp_basic/mod.rs
@@ -0,0 +1,3 @@
+pub fn tcp(){
+    println!("inside tcp folder");
+}
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,17 @@
 // mod variable;
 // mod function;
 // mod control_flow;
-mod ownership;
+// mod ownership;
+  mod extra {
+    pub mod tcp_basic;
+  }
+ 
 fn main() {
     println!("this is the main file !!!");
     // variable::variable();
     // function::fun();
-    ownership::ownership();
+    // control_flow::flow();
+    // ownership::ownership();
+    extra::tcp_basic::tcp();  
+    
 }
